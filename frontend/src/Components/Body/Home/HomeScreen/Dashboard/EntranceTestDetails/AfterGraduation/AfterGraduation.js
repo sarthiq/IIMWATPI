d1654b1b014e8647329d99453f23a521602fe36e
bipins867 <bipins867@gmail.com>
2025-03-18 15:36:29 +0530

    added the examiniation section ...

---
+++ frontend/src/Components/Body/Home/HomeScreen/Dashboard/EntranceTestDetails/AfterGraduation/AfterGraduation.js
@@ -0,0 +1,7 @@
+export const AfterGraduation=()=>{
+    return(
+        <div>
+            <h1>After Graduation</h1>
+        </div>
+    )
+}
---
+++ frontend/src/Components/Body/Home/HomeScreen/Dashboard/EntranceTestDetails/EntranceTestDetails.js
@@ -0,0 +1,18 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { EntranceHome } from "./EntranceHome/EntranceHome";
+import { IntermediateExam } from "./IntermediateExam/IntermediateExam";
+import { GraduationExam } from "./GraduationExam/GraduationExam";
+import { AfterGraduation } from "./AfterGraduation/AfterGraduation";
+
+export const EntranceTestDetails=()=>{
+    return(
+        
+        <Routes>
+            <Route path="/" element={<EntranceHome />} />
+            <Route path="/intermediate" element={<IntermediateExam />} />
+            <Route path="/graduation" element={<GraduationExam />} />
+            <Route path="/after-graduation" element={<AfterGraduation />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+    )
+}
