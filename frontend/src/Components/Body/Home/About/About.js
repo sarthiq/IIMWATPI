ec2193ee0146fadaa9289fdb951b810fea220f5b
Alok0325 <alokprajapati024@gmail.com>
2025-03-20 15:33:08 +0530

    Anout Us

--- frontend/src/Components/Body/Home/About/About.js
+++ frontend/src/Components/Body/Home/About/About.js
@@ -7,9 +7,9 @@ export const About = () => {
       <Container>
         <Row className="mb-5">
           <Col>
-            <h1 className="section-title">About Us</h1>
-            <p className="about-text">
-              <strong>Educating, enabling, and empowering students to achieve their dreams in the AI-driven world!</strong>
+            <h1 className="about-title">About Us</h1>
+            <p className="about-text-bold">
+              Educating, enabling, and empowering students to achieve their dreams in the AI-driven world!
             </p>
             <p className="about-text">
               We help students select courses based on their strengths, interests, and aspirations. We guide them to achieve the best education in their field of interest; simultaneously, we educate and equip students with AI skills to enhance their capabilities to become future-ready professionals.
